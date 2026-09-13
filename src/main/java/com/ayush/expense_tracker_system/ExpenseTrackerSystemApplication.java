66dc295228598ea068395318f4c1572204be21f1
rayush0325 <rayush2504@gmail.com>
2026-09-13 22:45:12 +0530

    chore: initialize Spring Boot backend and React + Vite frontend

---
+++ src/main/java/com/ayush/expense_tracker_system/ExpenseTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ayush.expense_tracker_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayush/expense_tracker_system/ExpenseTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.expense_tracker_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
