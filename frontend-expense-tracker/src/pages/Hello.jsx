a879e24e494f20b0280421a390f5861df7baebd4
rayush0325 <rayush2504@gmail.com>
2026-09-16 13:34:50 +0530

    feat(frontend) : handled error using try-catch block

--- frontend-expense-tracker/src/pages/Hello.jsx
+++ frontend-expense-tracker/src/pages/Hello.jsx
@@ -4,15 +4,24 @@ import apiClient from "../services/apiClient"
 export default function Hello() {
     const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        getData();
-    }, [])
+    useEffect(callback, [])
 
+    function callback() {
+        console.log("hello from callback");
+        getData();
+    }
 
     async function getData() {
-        const response = await apiClient.get('/hello');
-        setMessage(response.data.message);
-    }
+
+        try {
+            const response = await apiClient.get('/hello');
+            setMessage(response.data.message);
+        } catch (error) {
+            console.log(error.message);
+            setMessage(error.message);
+        }
+    }   
+
 
     return <h1>{message}</h1>
 }
