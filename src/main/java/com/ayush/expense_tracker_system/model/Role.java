0ecfd2d4015fe7019b3de1da2d867f092c72226f
rayush0325 <rayush2504@gmail.com>
2026-09-22 06:52:21 +0530

    feat(backend): create User entity mapped to users table - @Entity to declare class as a hibernate managed persistent entity - @Table to define table name
    - Create `User` entity class mapped to the `users` table
    
    - `id` field:
      - Type: `Long`, annotated with `@Id`
      - Unique identifier for each user
      - Generation strategy: `GenerationType.IDENTITY`
        - Supported natively by MySQL
        - No need to manage a separate DB sequence object
        - Does not support batch operations, which are not needed in our system
    
    - `email` field:
      - Used as the login identifier for uniquely identifying a user during login
      - Used for email services such as sending a forgot-password email
      - Standard and naturally unique value
      - Constraint: UNIQUE
    
    - `role` field:
      - Datatype: ENUM — fixed set of values required in the system
      - Default: `USER`
      - Defined a separate ENUM class to declare the enum values
    
    - `fullName` field: String
    - `password` field: String
    
    - All fields annotated with `@Column`
    - All fields set as NOT NULL
    - Default values will be set in the service layer
    - Used Lombok to generate getters and setters
    - table gets automatically created

---
+++ src/main/java/com/ayush/expense_tracker_system/model/Role.java
@@ -0,0 +1,5 @@
+package com.ayush.expense_tracker_system.model;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/ayush/expense_tracker_system/model/User.java
@@ -0,0 +1,32 @@
+package com.ayush.expense_tracker_system.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
